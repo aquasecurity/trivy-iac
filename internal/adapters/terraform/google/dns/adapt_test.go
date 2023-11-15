9c4e43c0688e457f2dd52e52ad9e101741856c27
Nikita Pivkin <nikita.pivkin@smartforce.io>
2023-11-15 17:01:32 +0700

    feat(google): improve AVD-GCP-0012 rule

--- internal/adapters/terraform/google/dns/adapt_test.go
+++ internal/adapters/terraform/google/dns/adapt_test.go
@@ -23,21 +23,25 @@ func Test_Adapt(t *testing.T) {
 		{
 			name: "basic",
 			terraform: `
-			resource "google_dns_managed_zone" "example" {
-				name        = "example-zone"
-				dns_name    = "example-${random_id.rnd.hex}.com."
-				description = "Example DNS zone"
-				labels = {
-				  foo = "bar"
-				}
-				dnssec_config {
-				  state = "on"
-				  default_key_specs {
-					  algorithm = "rsasha1"
-					  key_type = "keySigning"
-				  }
-				}
-			}
+resource "google_dns_managed_zone" "example" {
+  name        = "example-zone"
+  dns_name    = "example-${random_id.rnd.hex}.com."
+  description = "Example DNS zone"
+  labels = {
+    foo = "bar"
+  }
+  dnssec_config {
+    state = "on"
+    default_key_specs {
+      algorithm = "rsasha1"
+      key_type  = "keySigning"
+    }
+    default_key_specs {
+      algorithm = "rsasha1"
+      key_type  = "zoneSigning"
+    }
+  }
+}
 `,
 			expected: dns.DNS{
 				ManagedZones: []dns.ManagedZone{
@@ -46,15 +50,16 @@ func Test_Adapt(t *testing.T) {
 						Visibility: defsecTypes.String("public", defsecTypes.NewTestMetadata()),
 						DNSSec: dns.DNSSec{
 							Enabled: defsecTypes.Bool(true, defsecTypes.NewTestMetadata()),
-							DefaultKeySpecs: dns.KeySpecs{
-								Metadata: defsecTypes.NewTestMetadata(),
-								ZoneSigningKey: dns.Key{
-									Metadata:  defsecTypes.NewTestMetadata(),
-									Algorithm: defsecTypes.String("", defsecTypes.NewTestMetadata()),
-								},
-								KeySigningKey: dns.Key{
+							DefaultKeySpecs: []dns.KeySpecs{
+								{
 									Metadata:  defsecTypes.NewTestMetadata(),
 									Algorithm: defsecTypes.String("rsasha1", defsecTypes.NewTestMetadata()),
+									KeyType:   defsecTypes.String("keySigning", defsecTypes.NewTestMetadata()),
+								},
+								{
+									Metadata:  defsecTypes.NewTestMetadata(),
+									Algorithm: defsecTypes.String("rsasha1", defsecTypes.NewTestMetadata()),
+									KeyType:   defsecTypes.String("zoneSigning", defsecTypes.NewTestMetadata()),
 								},
 							},
 						},
@@ -68,46 +73,6 @@ func Test_Adapt(t *testing.T) {
 		t.Run(test.name, func(t *testing.T) {
 			modules := tftestutil.CreateModulesFromSource(t, test.terraform, ".tf")
 			adapted := Adapt(modules)
-			testutil.AssertDefsecEqual(t, test.expected, adapted)
-		})
-	}
-}
-
-func Test_adaptKeySpecs(t *testing.T) {
-	tests := []struct {
-		name      string
-		terraform string
-		expected  dns.KeySpecs
-	}{
-		{
-			name: "basic",
-			terraform: `
-
-			data "google_dns_keys" "foo_dns_keys" {
-				managed_zone = google_dns_managed_zone.example.id
-				zone_signing_keys {
-					algorithm = "rsasha512"
-				}
-			}
-`,
-			expected: dns.KeySpecs{
-				Metadata: defsecTypes.NewTestMetadata(),
-				ZoneSigningKey: dns.Key{
-					Metadata:  defsecTypes.NewTestMetadata(),
-					Algorithm: defsecTypes.String("rsasha512", defsecTypes.NewTestMetadata()),
-				},
-				KeySigningKey: dns.Key{
-					Metadata:  defsecTypes.NewTestMetadata(),
-					Algorithm: defsecTypes.String("", defsecTypes.NewTestMetadata()),
-				},
-			},
-		},
-	}
-
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			modules := tftestutil.CreateModulesFromSource(t, test.terraform, ".tf")
-			adapted := adaptKeySpecs(modules.GetBlocks()[0])
 			testutil.AssertDefsecEqual(t, test.expected, adapted)
 		})
 	}
@@ -140,9 +105,9 @@ func TestLines(t *testing.T) {
 	assert.Equal(t, 7, zone.DNSSec.Enabled.GetMetadata().Range().GetStartLine())
 	assert.Equal(t, 7, zone.DNSSec.Enabled.GetMetadata().Range().GetEndLine())
 
-	assert.Equal(t, 8, zone.DNSSec.DefaultKeySpecs.Metadata.Range().GetStartLine())
-	assert.Equal(t, 11, zone.DNSSec.DefaultKeySpecs.Metadata.Range().GetEndLine())
+	assert.Equal(t, 8, zone.DNSSec.DefaultKeySpecs[0].Metadata.Range().GetStartLine())
+	assert.Equal(t, 11, zone.DNSSec.DefaultKeySpecs[0].Metadata.Range().GetEndLine())
 
-	assert.Equal(t, 9, zone.DNSSec.DefaultKeySpecs.KeySigningKey.Algorithm.GetMetadata().Range().GetStartLine())
-	assert.Equal(t, 9, zone.DNSSec.DefaultKeySpecs.KeySigningKey.Algorithm.GetMetadata().Range().GetEndLine())
+	assert.Equal(t, 9, zone.DNSSec.DefaultKeySpecs[0].Algorithm.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 9, zone.DNSSec.DefaultKeySpecs[0].Algorithm.GetMetadata().Range().GetEndLine())
 }
